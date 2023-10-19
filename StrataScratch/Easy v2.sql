b299a38d1d5b7e69b4c28381d895c874f6716437
Awojide Margaret <170806003@live.unilag.edu.ng>
2023-10-19 11:54:06 +0100

    finding updated records problem

--- StrataScratch/Easy v2.sql
+++ StrataScratch/Easy v2.sql
@@ -92,3 +92,13 @@ Output all details related to retrieved records.
 SELECT * FROM lyft_drivers
 WHERE (yearly_salary <= 30000) OR (yearly_salary >= 70000);
 
+--finding updated records (Microsoft)
+/*We have a table with employees and their salaries, however, 
+some of the records are old and contain outdated salary information. 
+Find the current salary of each employee assuming that salaries increase each year. Output their id, first name, last name, department ID, and current salary. 
+Order your list by employee ID in ascending order.*/
+
+SELECT id,first_name, last_name, department_id, MAX(salary) AS max_salary
+FROM ms_employee_salary
+GROUP BY first_name, last_name,department_id,id
+ORDER BY id;
