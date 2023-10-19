b97deaf66febdee33306d22124fa095a04a65820
Awojide Margaret <170806003@live.unilag.edu.ng>
2023-10-19 11:49:18 +0100

    lyft_driver_wages

--- StrataScratch/Easy v2.sql
+++ StrataScratch/Easy v2.sql
@@ -85,3 +85,10 @@ FROM spotify_worldwide_daily_song_ranking
 GROUP BY artist
 ORDER BY n_occurences DESC;
 
+--Lyft Driver Wages (Lyft)
+/*Find all Lyft drivers who earn either equal to or less than 30k USD or equal to or more than 70k USD.
+Output all details related to retrieved records.
+*/
+SELECT * FROM lyft_drivers
+WHERE (yearly_salary <= 30000) OR (yearly_salary >= 70000);
+
